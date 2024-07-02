1f299593d12633e6edc9865bc4156de75e41e433
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2024-07-02 15:18:40 +0530

    Update Delay.swift

---
+++ Lists/Utils/Delay.swift
@@ -0,0 +1,32 @@
+//
+//  Delay.swift
+//  Lists
+//
+//  Created by Ayesha Khan on 02/07/24.
+//
+
+import Foundation
+
+class Delay {
+    
+    private var seconds: Double
+    var workItem: DispatchWorkItem?
+    
+    init(seconds: Double = 2.0) {
+        self.seconds = seconds
+    }
+    
+    func performWork(_ work: @escaping () -> Void) {
+        
+        workItem = DispatchWorkItem(block: {
+            work()
+        })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: workItem!)
+    }
+    
+    func cancel() {
+        workItem?.cancel()
+    }
+    
+}
